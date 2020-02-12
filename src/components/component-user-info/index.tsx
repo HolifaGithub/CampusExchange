8283a46dc43a9a8bd6c98bdfda7e85aa38e9d532
holifa <1261415444@qq.com>
2020-02-12 12:02:28 +0800

    feat:增加获取用户开放数据获取昵称头像信息

--- src/components/component-user-info/index.tsx
+++ src/components/component-user-info/index.tsx
@@ -1,17 +1,34 @@
 import Taro, { useState, useEffect } from '@tarojs/taro'
-import { View, Text, Image } from '@tarojs/components'
+import { View, Text, Image, Button, OpenData } from '@tarojs/components'
 import { CDNWebSite } from '../../static-name/web-site'
 import Skeleton from 'taro-skeleton'
 import './index.scss'
 
+function checkIsLogin(setIsLogin) {
+  Taro.getSetting({
+    success: function (res) {
+      console.log(res.authSetting)
+      // res.authSetting = {
+      //   "scope.userInfo": true,
+      //   "scope.userLocation": true
+      // }
+      if (res.authSetting["scope.userInfo"] === true) {
+        setIsLogin(true)
+      } else {
+        setIsLogin(false)
+      }
+    }
+  })
+}
 function UserInfo() {
   let [loading, setLoading] = useState(true)
-  let [nickName,setNickName]=useState('登录/注册')
+  let [isLogin, setIsLogin] = useState(false)
   useEffect(() => {
     setTimeout(() => {
       setLoading(false)
     }, 1000)
-  }, [])
+    checkIsLogin(setIsLogin)
+  }, [isLogin])
   return (
     <Skeleton
       row={1}
@@ -25,8 +42,26 @@ function UserInfo() {
           <Image src={`${CDNWebSite}/icon/user-info/qr-code.png`} className='qr-code-image'></Image>
         </View>
         <View className='user-info'>
-            <Image src='http://www.xiaoyuanhuan.xyz:3000/img/banner2.png' className='avatar'></Image>
-            <Text className='nick-name'>{nickName}</Text>
+          {!isLogin ? <Image src={`${CDNWebSite}/icon/user-info/default-avatar-white.png`} className='avatar'></Image> : <OpenData
+            type='userAvatarUrl'
+            default-avatar={`${CDNWebSite}/icon/user-info/default-avatar-white.png`}
+            className='avatar'
+          ></OpenData>}
+          {!isLogin ? null : <OpenData type='userNickName' className='nick-name'></OpenData>}
+          {isLogin ? null : <Button type='warn'
+            openType='getUserInfo'
+            onGetUserInfo={(res) => {
+              console.log(res.detail)
+              Taro.login({
+                success(res) {
+                  console.log(res)
+                  checkIsLogin(setIsLogin)
+                }
+              })
+            }
+            }
+            className='login-and-register'
+            >登录/注册</Button>}
         </View>
       </View>
     </Skeleton>
